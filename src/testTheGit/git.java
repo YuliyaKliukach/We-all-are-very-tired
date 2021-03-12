5eb8fdececb9cc1cfbf98f4bbf702a0eca9145e1
YuliyaKliukach <julia.klukach@gmail.com>
2021-03-11 20:29:08 -0500

    New file added

---
+++ src/testTheGit/git.java
@@ -0,0 +1,13 @@
+package testTheGit;
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I'm getting crazy");
+		
+		System.out.println("Whats up?");
+	}
+
+}
